b636ef49773655e3b529af8b99c9559e59510a7d
DUBAI_KEY <jrjb079@gmail.com>
2022-06-19 21:31:30 +0200

    android ui as (colors,fonts and theme)

--- androidApp/src/main/java/com/example/foodfor_kmm/android/presentation/components/EmptyScreen.kt
+++ androidApp/src/main/java/com/example/foodfor_kmm/android/presentation/components/EmptyScreen.kt
@@ -1,14 +1,18 @@
 package com.example.foodfor_kmm.android.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.example.foodfor_kmm.android.common.Constants
 
 @Composable
 fun EmptyScreen(
@@ -19,12 +23,13 @@ fun EmptyScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
 
-//        Image(painter = painterResource(id = com.example.foodfor_kmm.android.R.drawable.ic_empty),
-//            contentDescription = null,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(Constants.RECIPE_IMAGE_HEIGHT.dp),
-//            contentScale = ContentScale.Crop)
+        Image(painter = painterResource(id = com.example.foodfor_kmm.android.R.drawable.ic_empty),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+                .height(Constants.RECIPE_IMAGE_HEIGHT.dp),
+            contentScale = ContentScale.Crop)
         Spacer(modifier = Modifier
             .fillMaxWidth()
             .height(10.dp))
